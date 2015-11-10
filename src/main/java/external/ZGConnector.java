766ace46412429763b46ebbd8b195643c7f0d59f
Ken Danniswara <ken.dannis@gmail.com>
2015-11-10 12:49:32 +0100

    add ZGConnector

---
+++ src/main/java/external/ZGConnector.java
@@ -0,0 +1,28 @@
+package external;
+
+import java.util.HashMap;
+
+import org.mortbay.util.MultiMap;
+import core.Cloud;
+
+public abstract class ZGConnector {
+
+	protected MultiMap tasksByCloudName = new MultiMap();
+	
+	public abstract void writeInfo();
+	public abstract MultiMap readInfo();
+	
+	public void addInfo(HashMap<String,Cloud> clouds) {
+		
+		for(Cloud c : clouds.values())
+		{
+			String cloudName = (String) c.getName() + ";";
+			
+			if(!c.getTasks().isEmpty())
+			{
+				for(Integer t : c.getTasks())
+					tasksByCloudName.add(cloudName, t);	
+			}
+		}
+	}
+}
